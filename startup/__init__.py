26452e84b4d761428b511b4098abf292ef8d1eb3
illuzX <98640218+illuzX@users.noreply.github.com>
2022-03-26 22:23:53 +0530

    Add files via upload

---
+++ startup/__init__.py
@@ -0,0 +1,8 @@
+# (c) illuzX
+# = Kang With Credits***
+
+from dotenv import load_dotenv
+import startup.client
+
+load_dotenv()
+bot = startup.client.Client()
---
+++ startup/__main__.py
@@ -0,0 +1,7 @@
+# (c) illuzX
+
+import startup
+
+
+if __name__ == "__main__":
+    bot.bot.run()

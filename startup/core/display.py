447147fe8efc83e038d1af3540576e62005510b3
illuzX <98640218+illuzX@users.noreply.github.com>
2022-03-26 22:39:54 +0530

    update display.py

--- startup/core/display.py
+++ startup/core/display.py
@@ -4,7 +4,7 @@
 import math
 import time
 import asyncio
-import bot
+import startup
 from typing import Union
 from pyrogram.types import Message, CallbackQuery
 from pyrogram.errors import FloodWait

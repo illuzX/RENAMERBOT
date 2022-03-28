fd1849cc84b3c605cb4f25e91bdfaf4fdd1e37c9
illuzX <98640218+illuzX@users.noreply.github.com>
2022-03-28 09:50:36 +0530

    update on_media.py

--- startup/tools/on_media.py
+++ startup/tools/on_media.py
@@ -1,6 +1,28 @@
 # (c) illuzX 
 ##1ea4c6
-
+import time
+import mimetypes
+import traceback
+from startup.client import (
+    Client
+)
+from pyrogram import filters
+from pyrogram.file_id import FileId
+from pyrogram.types import Message
+from startup.core.file_info import (
+    get_media_file_id,
+    get_media_file_size,
+    get_media_file_name,
+    get_file_type,
+    get_file_attr
+)
+from configs import Config
+from startup.core.display import progress_for_pyrogram
+from startup.core.db.database import db
+from startup.core.db.add import add_user_to_database
+from startup.core.handlers.not_big import handle_not_big
+from startup.core.handlers.time_gap import check_time_gap
+from startup.core.handlers.big_rename import handle_big_rename
 import asyncio
 from startup.client import Client
 from startup.core.db.add import (
